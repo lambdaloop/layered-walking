b876cea42d1b3957ffce4c2bbf348db4e9dbc4b3
Lisa Li <flyingpeach@live.com>
2022-04-26 17:58:21 -0700

    Get symbolic dynamics for leg

--- leg_dynamics/test.py
+++ leg_dynamics/test.py
@@ -5,17 +5,19 @@
 import sympy
 
 # Install from https://github.com/cdsousa/SymPyBotics
+# Some customizations required
 import sympybotics
 
+# Parameters
+L1 = 1
+L3 = 1
+L4 = 1
+
+mCoxa  = 1
+mFemur = 1
+mTibia = 1
+
 # DH parameter order: (alpha, a, d, theta)
-
-# This is a robot we have dynamics for; sanity check using this one
-rbtdef = sympybotics.RobotDef('ReferenceRobot',
-                              [('-pi/2', 0, 'q1',   0),
-                               (      0, 1,    0,'q2')],
-                              dh_convention='standard')
-rbtdef.frictionmodel = None
-
 legdef = sympybotics.RobotDef('LegRobot',
                               [(      0, L1,  0, 'q1'),
                                ('-pi/2',  0,  0, 'q2'),
@@ -24,14 +26,27 @@ legdef = sympybotics.RobotDef('LegRobot',
                               dh_convention='standard')
 legdef.frictionmodel = None
 
+# Custom definition of inertia
+Le = [None] * 4
+for i in range(len(Le)):
+    Le[i] = [0] * 6 
+    # Convention: [L_xx, L_xy, L_xz, L_yy, L_yz, L_zz]
+
+Le[0][5] = 1.0/3.0 * mCoxa * L1 * L1  # L1_zz
+Le[2][3] = 1.0/3.0 * mFemur * L3 * L3 # L3_yy
+Le[3][5] = 1.0/3.0 * mTibia * L4 * L4 # L4_zz
+
+legdef.set_Le(Le)
+legdef.set_l_zero() # Ignore first moment of inertia
 
 # Generate dynamics
-rbt = sympybotics.RobotDynCode(rbtdef, verbose=True)
+leg = sympybotics.RobotDynCode(legdef, verbose=True)
+
 
 # M(q)*qdotdot + C(q,qdot)qdot + g(q) = tau
 # (Ignores non-rotational forces)
 
-# Inertia matrix,  symbolic: rbt.dyn.M, formatted: rbt.M_code
-# Coriolis matrix, symbolic: rbt.dyn.C, formatted: rbt.C_code
-# Gravity term,    symbolic: rbt.dyn.g, formatted: rbt.g_code
- 
+# Inertia matrix,  symbolic: dyn.M, formatted: M_code
+# Coriolis matrix, symbolic: dyn.C, formatted: C_code
+# Gravity term,    symbolic: dyn.g, formatted: g_code
+
