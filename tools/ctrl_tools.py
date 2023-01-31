abdbb7d762026322f5c3aecdefb14ff1fbd02e94
Lisa Li <flyingpeach@live.com>
2023-01-31 15:56:59 -0800

    Integrate sensor delay augmentation into toolbox

--- tools/ctrl_tools.py
+++ tools/ctrl_tools.py
@@ -214,61 +214,89 @@ def get_linearized_system(DHTable, linkMasses, inertias, frics, xEqm, leg):
 
 
 
-def get_augmented_system(AReal, BReal, numDelays):
-    ''' Augment system to include delayed actuation and lookahead '''
-    if numDelays == 0:
-        return (AReal, BReal)
-    Nx = BReal.shape[0]
-    Nu = BReal.shape[1]
-        
-    # Dimensions of blocks
-    N1 = Nx*(numDelays+1)
-    N2 = Nu*numDelays
-    NA = 2*N1 + N2
-        
-    A     = np.zeros([NA, NA])
-    ANow1 = np.eye(Nx) # Powers of A for upper left block of A
-    ANow2 = np.eye(Nx) # Powers of A for upper middle block of A
-    ABNow = BReal      # Powers of A*B for upper right block of A
-        
-    for i in range(numDelays+1):
-        # Upper left block of A
-        ANow1 = AReal @ ANow1
-        A[Nx*i:Nx*(i+1), 0:Nx] = ANow1
-        
-        # Upper middle block of A
-        aux     = np.empty([Nx*i, 0])
-        blkdiag = aux # Be careful, shallow copy
-        for j in range(numDelays+1-i):
-            blkdiag = block_diag(blkdiag, ANow2)
-        blkdiag = block_diag(blkdiag, aux.T)
-        ANow2   = AReal @ ANow2
-        A[0:N1, N1:2*N1] += blkdiag
-        
-        # Upper right block of A
-        for j in range(numDelays):
-            rowStart = (i+j)*Nx
-            rowEnd   = rowStart + Nx
-            if rowEnd <= N1:
-                colStart = 2*N1+(numDelays-j-1)*Nu
-                colEnd   = colStart + Nu            
-                A[rowStart:rowEnd, colStart:colEnd] = ABNow
-        ABNow = AReal @ ABNow            
-
-    # Middle block of A
-    aux                 = np.empty([0, Nx])
-    A[N1:2*N1, N1:2*N1] = block_diag(aux, np.eye(Nx*numDelays), aux.T)
-
-    # Bottom right block of A
-    aux                 = np.empty([Nu, 0])
-    A[2*N1:NA, 2*N1:NA] = block_diag(aux, np.eye(Nu*(numDelays-1)), aux.T)
-
-    # Construct B matrix
-    B                 = np.zeros([NA, Nu])
-    B[N1-Nx:N1,:]     = BReal
-    B[2*N1:2*N1+Nu,:] = np.eye(Nu)
+def get_augmented_system(AReal, BReal, CReal, dSense, dAct):
+    ''' Augment system (AReal, BReal, CReal) to (A, B, C)
+        to include delayed sensing, delayed actuation, and lookahead '''
     
-    return (A, B)
+    if dSense == 0 and dAct == 0:
+        return (AReal, BReal, CReal)
+    
+    # Original dimensions: state, input, sensor
+    n = BReal.shape[0]
+    m = BReal.shape[1]
+    p = CReal.shape[0]
+        
+    # Block dimensions of augmented A
+    n1 = n*(dAct+1) # real state + lookahead states
+    n2 = m*dAct     # act delay states
+    n3 = p*dSense   # sense delay states
+    n4 = n*(dAct+1) # trajectory states
+        
+    nx = n1 + n2 + n3 + n4
+    nu = m + n3 # allow internal compensation to delayed sensors
+
+    A11  = np.zeros([n1, n1])
+    A11h = np.eye(n)
+    A12  = np.zeros([n1, n2])
+    A12h = BReal
+    A14  = np.zeros([n4, n4])
+    A14h = np.eye(n)
+
+    for i in range(dAct+1):
+        A11h = AReal @ A11h
+        A11[n*i:n*(i+1), 0:n] = A11h
+
+        for j in range(dAct):
+            row = (i+j)*n
+            if row + n <= n1:
+                col = (dAct-j-1)*m
+                A12[row:row+n, col:col+m] = A12h
+        A12h = AReal @ A12h            
+
+        aux     = np.empty([n*i, 0])
+        blkdiag = aux # Be careful, shallow copy
+        for j in range(dAct+1-i):
+            blkdiag = block_diag(blkdiag, A14h)
+        blkdiag = block_diag(blkdiag, aux.T)
+        A14h    = AReal @ A14h
+        A14     += blkdiag
+
+    aux = np.empty([m, 0])
+    A22 = np.empty([n2, n2])
+    if dAct > 0:
+        A22 = block_diag(aux, np.eye(m*(dAct-1)), aux.T)
+
+    A31 = np.zeros([n3, n1])
+    if dSense > 0:
+        A31[0:p,0:n] = CReal
+
+    aux = np.empty([p, 0])
+    A33 = np.empty([n3, n3])
+    if dSense > 0:
+        A33 = block_diag(aux, np.eye(p*(dSense-1)), aux.T)
+
+    aux = np.empty([0, n])
+    A44 = block_diag(aux, np.eye(n*dAct), aux.T)
+
+    # Put A together row-wise
+    A1 = np.concatenate([A11, A12, np.zeros([n1,n3]), A14], axis=1)
+    A2 = np.concatenate([np.zeros([n2,n1]), A22, np.zeros([n2,n3+n4])], axis=1)
+    A3 = np.concatenate([A31, np.zeros([n3,n2]), A33, np.zeros([n3,n4])], axis=1)
+    A4 = np.concatenate([np.zeros([n4,n1+n2+n3]), A44], axis=1)
+    A  = np.concatenate([A1, A2, A3, A4]);
+
+    B  = np.zeros([nx, nu])
+    B[n*dAct:n1, 0:m] = BReal
+    B[n1:n1+m, 0:m]   = np.eye(m)
+    B[n1+n2:n1+n2+n3, m:nu] = np.eye(p*dSense)  
+
+    C  = np.zeros([p, nx])
+    if dSense > 0: # Access delayed sensor reading
+        C[:,n1+n2+p*(dSense-1):n1+n2+n3] = np.eye(p)
+    else: # Access sensor directly; no delay
+        C[:,0:n] = CReal
+
+    return (A, B, C)
 
 
 
@@ -282,6 +310,7 @@ def get_augmented_dist_mtx(AReal, numDelays):
     return mtx       
 
 
+# TODO: update control and dynamics
 
 class ControlAndDynamics:
     def __init__(self, leg, Ts, numDelays, futurePenRatio, anglePen, drvPen, inputPen, namesTG=None):
